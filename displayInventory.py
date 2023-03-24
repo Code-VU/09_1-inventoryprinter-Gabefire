68fb8c58beec16d2cad7ea8797fa35ff4c776257
Gabe Underwood <Gabe.Underwood@vu.com>
2023-03-24 11:27:29 -0500

    First try

--- displayInventory.py
+++ displayInventory.py
@@ -1,8 +1,8 @@
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
 def displayInventory(inventory):
-    # your code goes here
-
-
+    print('Inventory:')
+    [print(y, x) for x, y in inventory.items()]
+    print(f'Total number of items: {sum(inventory.values())}')
 if __name__ == "__main__":
     displayInventory(stuff)
